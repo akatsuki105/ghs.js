ff23e5d1c99306fc4dc7e4d50f3ae19312b59e04
pokemium <bluejapan73+dev@gmail.com>
2022-09-07 02:28:12 +0400

    f

--- src/Bpp.tsx
+++ src/Bpp.tsx
@@ -3,33 +3,22 @@ import { Separator, Spacer, ROMUpload, Box, Rom, FlexBox, TileViewer } from './c
 import { useWindowDimensions } from './hooks';
 import { convert4BppToRGB, palettes, RGB, rgb555 } from './utils';
 
-const ROM = 0x0800_0000 as const;
+// const ROM = 0x0800_0000 as const;
 
 export const Bpp: React.VFC = React.memo(() => {
   const [rgb, setRgb] = useState<RGB[]>([]);
   const [width, _] = useState<number>(16);
-  const [addr, setAddr] = useState<number>(ROM);
   const { height } = useWindowDimensions();
 
   const scale = 4;
   const canvasHeight = ((height * 9) / 10) & ~(8 * scale - 1);
-  const onScroll = (x: number, y: number) => {
-    setAddr(width * y * 32 + 32 * x + ROM);
-  };
 
   return (
     <FlexBox className="w-full">
       <div className="w-1/2">
         {rgb.length > 0 ? (
           <div className="flex justify-center">
-            <TileViewer
-              w={width * 8}
-              h={canvasHeight}
-              rgb={rgb}
-              scale={scale}
-              onScroll={onScroll}
-              grid
-            />
+            <TileViewer w={width * 8} h={canvasHeight} rgb={rgb} scale={scale} grid />
           </div>
         ) : (
           <Box height={canvasHeight} />
@@ -52,7 +41,7 @@ export const Bpp: React.VFC = React.memo(() => {
             />
           </>
         ) : (
-          <div>{`Address: 0x${addr.toString(16)}`}</div>
+          <div>{`Address: 0x`}</div>
         )}
       </div>
     </FlexBox>
